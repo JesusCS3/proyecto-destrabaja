38000fe98d28f907d8c8ac2a03e3ad82b5913e44
JesusCS3 <jesus.colmser@gmail.com>
2022-12-01 21:56:54 -0600

    arreglos botones de ayuda, textos de prueba (presupuesto) proyecto, tamaño fijo de columnas servicio

--- src/app/pages/user/profile/profile-settings/profile-settings/profile-settings.component.ts
+++ src/app/pages/user/profile/profile-settings/profile-settings/profile-settings.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 import { ProfileSettingsService } from '../services/profile-settings.service';
@@ -6,7 +6,35 @@ import { ProfileSettingsService } from '../services/profile-settings.service';
 @Component({
   selector: 'app-profile-settings',
   templateUrl: './profile-settings.component.html',
-  styleUrls: ['./profile-settings.component.css']
+  styleUrls: ['./profile-settings.component.css'],
+  encapsulation: ViewEncapsulation.None,
+	styles: [
+		`
+			.my-custom-class .tooltip-inner {
+				background-color: #fff;
+				font-size: 0.8rem;
+        font-weight: bold;
+        color: #425CC7;
+        text-align: justify;
+        padding: 10px;
+        margin: 5px;
+        max-width: 310px !important;
+        box-shadow: rgba(0, 0, 0, 0.301) 0px 2px 4px 0px, rgba(0, 0, 0, 0.301) 0px 2px 16px 0px !important;
+			}
+			.my-custom-class.bs-tooltip-end .tooltip-arrow::before {
+				border-right-color: #fff;
+			}
+			.my-custom-class.bs-tooltip-start .tooltip-arrow::before {
+				border-left-color: #fff;
+			}
+			.my-custom-class.bs-tooltip-top .tooltip-arrow::before {
+				border-top-color: #fff;
+			}
+			.my-custom-class.bs-tooltip-bottom .tooltip-arrow::before {
+				border-bottom-color: #fff;
+			}
+		`,
+  ]
 })
 export class ProfileSettingsComponent implements OnInit {
 
